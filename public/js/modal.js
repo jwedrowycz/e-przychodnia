0ce61c5651be870b69a576c668edbfc68db15e97
jwedrowycz <jacob98k@hotmail.com>
2020-11-02 01:17:18 +0100

    poprawka modal

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,5 +1,5 @@
    // Get the modal
-   var modal = document.getElementById("modalOpen");
+   var modal = document.getElementById("modalWindow");
 
    // Get the button that opens the modal
    var btn = document.getElementById("modalBtn");
